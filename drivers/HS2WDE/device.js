e4e52a06f418a89e708a4331add23589809bcc3b
Marco Ruiter <mruiter@tuxweb.nl>
2021-05-03 20:31:01 +0200

    Update device.js

--- drivers/HS2WDE/device.js
+++ drivers/HS2WDE/device.js
@@ -12,9 +12,20 @@ class HS2WDE extends ZigBeeDevice {
   async onNodeInit({ zclNode }) {
     this.enableDebug(); // only for debugging purposes
     this.printNode(); // only for debugging purposes
-    // Register measure_battery capability and configure attribute reporting
-    this.batteryThreshold = 20;
-    this.registerCapability('alarm_battery', CLUSTER.POWER_CONFIGURATION, {
+	this.registerCapability('alarm_battery', CLUSTER.POWER_CONFIGURATION, {
+      getOpts: {
+        getOnStart: true,
+      },
+      reportOpts: {
+        configureAttributeReporting: {
+          minInterval: 0, // No minimum reporting interval
+          maxInterval: 60000, // Maximally every ~16 hours
+          minChange: 5, // Report when value changed by 5
+        },
+      },
+    });
+
+    this.registerCapability('measure_battery', CLUSTER.POWER_CONFIGURATION, {
       getOpts: {
         getOnStart: true,
       },
